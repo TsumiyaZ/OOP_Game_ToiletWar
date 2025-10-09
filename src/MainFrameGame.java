670b76afd9444ecffd05d0415f1dc875b5506ea1
TsumiyaZ <watcharakornnigc@gmail.com>
2025-10-10 01:33:23 +0700

    a

---
+++ src/MainFrameGame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class MainFrameGame extends JFrame {
+    public MainFrameGame() {
+        setFrame();
+    }
+
+    private void setFrame() {
+        setSize(1000, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+    }
+
+
+}
