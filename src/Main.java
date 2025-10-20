24c6a1c0831f9c0c50b69fa21dea653f2bdaedb1
TsumiyaZ <watcharakornnigc@gmail.com>
2025-10-20 18:39:40 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MainFrameGame frame = new MainFrameGame();
-        frame.setVisible(false);
+        frame.setVisible(true);
+        System.out.println("Kuy viego");
     }
 }
