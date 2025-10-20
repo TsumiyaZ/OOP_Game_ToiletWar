1f0b9d57b885132dc8021e675b4920dd59330bec
MyDesktop\VIEGo <vigothirapat2548@gmail.com>
2025-10-20 18:38:58 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         MainFrameGame frame = new MainFrameGame();
-        frame.setVisible(true);
+        frame.setVisible(false);
     }
 }
